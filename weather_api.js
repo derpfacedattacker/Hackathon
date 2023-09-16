2b2a9f3248ae268707050d94c48ed3abcdeedee1
alexmantagas <145210737+alexmantagas@users.noreply.github.com>
2023-09-16 11:34:29 -0400

    Update weather_api.js

--- weather_api.js
+++ weather_api.js
@@ -1,1 +1,1 @@
-https://api.openweathermap.org/data/3.0/onecall?lat={43.1566}&lon={77.6088}&exclude={None}&appid={6b3ecfe89a1df492b9e8cd9a0a5e2019}
+https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&appid=6b3ecfe89a1df492b9e8cd9a0a5e2019
